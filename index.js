ed339b980ee7aa2826336a0c8523598b01492ebd
Scott Weinstein <sweinstein27@gmail.com>
2018-10-31 15:04:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function loadIssue() {
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
-  for(var i=0, i<issues.length,i+++) {
+  for(var i=0;i<issues.length;i++) {
     var result = template(issues[i]);
     document.getElementsByTagName("main")[0].innerHTML += result;
   }
